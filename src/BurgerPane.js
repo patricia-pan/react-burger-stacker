2888ddcb45e9eff24aaa6469aff360251dc1bc42
Your Name <you@example.com>
2021-02-08 21:15:42 -0800

    Refactored class-based React to function-based React.

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react'
 import BurgerStack from './BurgerStack'
 import ClearBurger from './ClearBurger'
 
-class BurgerPane extends Component {
-    ingredients = this.props.ingredients
-    render() {
-        return (
-            <div>
-                <BurgerStack ingredients={this.props.ingredients}/>
-                
-                <ClearBurger clearBurger={this.props.clearBurger}/>
-            </div>
-
-        )
-    }
-
+function BurgerPane(props) {
+    let ingredients = props.ingredients
+    return (
+        <div>
+            {/* Not sure why reverse isn't working; it's not reverse the order in which the burger gets rendered. */}
+            <BurgerStack ingredients={props.ingredients}/>
+            
+            <ClearBurger clearBurger={props.clearBurger}/>
+        </div>
+    )
 }
 
 export default BurgerPane
--- src/IngredientsList.js
+++ src/IngredientsList.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react'
 import Ingredient from './Ingredient.js'
 
-class IngredientsList extends Component {
-    ingredients = this.props.ingredients
-    render() {
-        let ingredients = this.ingredients.map( (ingredient, i) => {
-            return (
-                <div>
-                    <Ingredient ingredient={ingredient} newIngredientChange={this.props.newIngredientChange} key={`ingredientKey ${i}`} />
-                    <button onClick={() => this.props.newIngredientChange(ingredient)}>Add to your burger</button>
-                </div>
-                )
-        })
-        
-        // comment
-        
+function IngredientsList(props) {   
+    let newIngredientChange = props.newIngredientChange
+    let ingredients = props.ingredients.map( (ingredient, i) => {
         return (
             <div>
-                {ingredients}
+                <Ingredient ingredient={ingredient} newIngredientChange={props.newIngredientChange} key={`ingredientKey ${i}`} />
+                <button onClick={() => newIngredientChange(ingredient)}>Add to your burger</button>
             </div>
             )
-    }
+    })
+        
+    return (
+        <div>
+            {ingredients}
+        </div>
+        )
         
 }
 
