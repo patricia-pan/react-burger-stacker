16a0ae2545100906d55b7bc1bcb66032c754778a
Your Name <you@example.com>
2021-02-08 16:47:15 -0800

    Color coding now works, information has to be in render and not before.

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 class Ingredient extends Component {
+    // Before, when we said color=this.props.ingredient.color, React was viewing this as a method as opposed to an assignment.
+    // It also wasn't being updated whenever stuff was re-rendered, since it was outside the render function.
     render() {
         let color = this.props.ingredient.color
         // console.log('Ingredient.js color: ', this.color)
